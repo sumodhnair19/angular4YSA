904e0c33c1cfb06ea4f7f5aeab737503a854d37e
SAPIENT\sgu124 <sgupta133@sapient.com>
2017-08-30 12:00:23 +0530

    Updated for adding a checkbox

--- angular4ysa/src/app/mock-data.ts
+++ angular4ysa/src/app/mock-data.ts
@@ -1,5 +1,6 @@
 import { TableData } from './table.format';
 
 export const Data: TableData[] = [
-  { id: 11, name: 'Mr. Nice', navigation: {content: "Google", url:"google.com"}}
+  { id: 11, name: 'Mr. Nice', navigation: {content: "Google", url:"google.com"}, combo: {checkStatus: "checked", labelText: "Checkbox 1"}},
+  { id: 12, name: 'Mr. Nic', navigation: {content: "Facebook", url:"facebook.com"}, combo: {checkStatus: "", labelText: "Checkbox 2"}}
 ];
--- angular4ysa/src/app/table.format.ts
+++ angular4ysa/src/app/table.format.ts
@@ -2,9 +2,14 @@ export class Navigation{
   content: string;
   url: string;
 }
+export class Checkbox{
+  checkStatus: string;
+  labelText: string;
+}
 export class TableData {
   id: number;
   name: string;
   navigation: Navigation;
+  combo: Checkbox;
 }
 
